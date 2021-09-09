b4622cc5ed21ef6d05bdf93cf60ba5d79a6c85a6
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2021-09-09 11:47:38 -0500

    upload all files

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React,{ useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    const data = await fetch(`https://rickandmortyapi.com/api/character`) 
+    const user = await data.json()
+    setItems(user.results)
+  }
+  console.log(items);
+  
+  const people = items.map(item => (
+    <Square key={item.id}>
+      <Title>
+        {item.name}
+      </Title>
+
+      <FlexInfo>
+        <FigureStatus style={{background: item.status === "Alive" ? "#00FF00" :
+            item.status === "Dead" ?"#FF0000" :
+              "#808080"}} />
+        <InfoCharacter>
+          {item.species} - {item.gender}
+        </InfoCharacter>
+      </FlexInfo>
+      
+      <div>
+        <Location>
+          Possible location:
+        </Location>
+        <DataLocation>
+          {item.location.name}
+        </DataLocation>
+      </div>
+      <p>{item.episode.name}</p>
+    </Square>
+  ))
+
+  return (
+    <List>
+      {people}
+    </List>
+  )
+}
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`
+
+const Square = styled.div`
+  margin: 10px;
+  text-align: center;
+  border: 2px solid #0089FF;
+  border-radius: 10px;
+`
+
+const Title = styled.h1`
+  color: #D6DBFF;
+`
+
+const FlexInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+`
+
+const FigureStatus = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 5px;
+`
+
+const InfoCharacter = styled.p`
+  color: #FFF;
+`
+
+const Location = styled.h3`
+  color: #129EAC;
+`
+
+const DataLocation = styled.p`
+  color: #ADEF12;
+`
+
+
+export default App;
